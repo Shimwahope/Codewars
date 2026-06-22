6648a33a9e1aadbfe9a366c0d0b6b573a62826de
Shimwa Hope <hoshimwa@gmail.com>
2026-06-22 15:43:03 +0200

    Added the question

--- ES6/Exercises/swap.js
+++ ES6/Exercises/swap.js
@@ -1,3 +1,4 @@
+//Write a function that takes in an array and swaps the positions of the first two elements in an array using destructuring, with default parameters to handle cases where input elements might be missing. The function should return a new array with the swapped elements.
 function swap([a,b,...rest]=[]){
     return [b,a,...rest]
 }
